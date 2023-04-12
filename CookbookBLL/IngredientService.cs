e6b43fe31b0f24b228ad16777e4c6d4f2adfd0cc
Jakub Drózd <jakudro043@student.polsl.pl>
2023-04-12 21:07:08 +0200

    Ingredient Service & Category Service

---
+++ CookbookBLL/IngredientService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CookbookBLL.Interfaces;
+using CookbookLibrary.Entities;
+using CookbookLibrary.Repositories;
+using CookbookLibrary.RepositoryInterfaces;
+
+namespace CookbookBLL
+{
+    internal class IngredientService : IIngredientService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public IngredientService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Ingredient>> GetAll()
+        {
+            var recipes = await _unitOfWork.IngredientRepository.GetAsync(includeProperties: "IngredientRecipes");
+            return recipes;
+        }
+
+        public async Task<Ingredient> GetIngredientById(int ingredientId)
+        {
+            var ingredient = _unitOfWork.IngredientRepository.GetByID(ingredientId);
+            return await Task.FromResult(ingredient);
+        }
+
+        public async Task Update(Ingredient ingredient)
+        {
+            try
+            {
+                _unitOfWork.IngredientRepository.Update(ingredient);
+                await _unitOfWork.SaveAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred while updating ingredient");
+                throw;
+            }
+        }
+
+        public async Task Add(Ingredient ingredient)
+        {
+            try
+            {
+                var workRepos = _unitOfWork.IngredientRepository;
+                workRepos.Insert(ingredient);
+                _unitOfWork.Save();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An Error occured while adding ingredient");
+                throw;
+            }
+        }
+
+        public async Task Delete(int ingredientId)
+        {
+            try
+            {
+                var workRepos = _unitOfWork.IngredientRepository;
+                workRepos.Delete(ingredientId);
+                await _unitOfWork.SaveAsync();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occured while deleting recipe");
+                throw;
+            }
+        }
+    }
+}
---
+++ CookbookBLL/Interfaces/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using CookbookLibrary.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookbookBLL.Interfaces
+{
+    internal interface ICategoryService
+    {
+        Task<IEnumerable<Category>> GetAll();
+        Task<Category> GetCategoryById(int categoryId);
+        Task Update(Category category);
+        Task Add(Category category);
+        Task Delete(int categoryId);
+    }
+}
