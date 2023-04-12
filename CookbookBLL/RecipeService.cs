0102b20d9901e81c224b5df05b04a28d7241386e
Piotr Kowalczyk <piotkow133@student.polsl.pl>
2023-04-12 21:45:01 +0200

    recipeService  modified

--- CookbookBLL/RecipeService.cs
+++ CookbookBLL/RecipeService.cs
@@ -11,6 +11,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using CookbookLibrary;
 
 namespace CookbookBLL
 {
@@ -31,7 +32,7 @@ namespace CookbookBLL
         public async Task<Recipe> GetRecipeById(int recipeId)
         {
             var recipe = _unitOfWork.RecipeRepository.GetByID(recipeId);
-            return await Task.FromResult(recipe);
+            return await Task.FromResult(recipe);  
         }
 
         public async Task Update(Recipe recipe)
@@ -96,6 +97,46 @@ namespace CookbookBLL
             }
         }
 
+        public async Task<IEnumerable<Recipe>> GetRecipesByCategory(int categoryId)
+        {
+            try
+            {
+                var recipes = await _unitOfWork.RecipeRepository
+                    .GetAsync(r => r.CategoryRecipes.Any(i => i.categoryId == categoryId),
+                        includeProperties: "Categories");
+
+                return recipes;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An errror occured while getting recipes");
+                throw;
+            }
+        }
+
+        public async Task AddCategoryToRecipe(int recipeId, Category category)
+        {
+
+            var recipes = await _unitOfWork.RecipeRepository.GetAsync(r => r.recipeId == recipeId,
+                                                          includeProperties: "CategoryRecipes");
+            var recipe = recipes.SingleOrDefault();
+                                                         
+                if (recipe == null)
+                {
+                    throw new Exception("Recipe not found");
+                }
+
+                var existingCategoryRecipe = recipe.CategoryRecipes
+                                                   .SingleOrDefault(cr => cr.categoryId == category.categoryId);
+                if (existingCategoryRecipe != null)
+                {
+                    throw new Exception("Category already assigned to recipe");
+                }
+
+                var categoryRecipe = new CategoryRecipe { Recipe = recipe, Category = category };
+                _unitOfWork.CategoryRecipeRepository.Insert(categoryRecipe);
+                await _unitOfWork.SaveAsync();
+        }
 
     }
 }
