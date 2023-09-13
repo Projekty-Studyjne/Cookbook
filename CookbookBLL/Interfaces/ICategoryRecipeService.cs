386c823497b04c796a8a40b8e17ff70e02d66c6c
alicluczak <alicluc654@student.polsl.pl>
2023-09-13 22:37:23 +0200

    add controllers, services and interfaces (CategoryRecipes, UserRecipes, IngredientsRecipes)

---
+++ CookbookBLL/Interfaces/ICategoryRecipeService.cs
@@ -0,0 +1,18 @@
+﻿using CookbookLibrary.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookbookBLL.Interfaces
+{
+    public interface ICategoryRecipeService
+    {
+        Task<IEnumerable<CategoryRecipe>> GetAll();
+        Task<CategoryRecipe> GetCategoryRecipeById(int categoryRecipeId);
+        Task Update(CategoryRecipe categoryRecipe);
+        Task Add(CategoryRecipe categoryRecipe);
+        Task Delete(int categoryRecipeId);
+    }
+}
---
+++ CookbookMVCBLL/Controllers/IngredientRecipesController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CookbookLibrary;
+using CookbookLibrary.Entities;
+using CookbookBLL.Interfaces;
+using System.Data;
+
+namespace CookbookMVCBLL.Controllers
+{
+    public class IngredientRecipesController : Controller
+    {
+        private IIngredientRecipeService service;
+
+        public IngredientRecipesController(IIngredientRecipeService service)
+        {
+            this.service = service;
+        }
+
+        public async Task<ViewResult> Index()
+        {
+            var ingredientRecipe = await service.GetAll();
+            return View(ingredientRecipe.ToList());
+        }
+
+        public async Task<ViewResult> Details(int id)
+        {
+            IngredientRecipe ingredientRecipe = await service.GetIngredientRecipeById(id);
+            return View(ingredientRecipe);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ingredientId,recipeId,quantity,unit")] IngredientRecipe ingredientRecipe)
+        {
+
+            service.Add(ingredientRecipe);
+            return RedirectToAction(nameof(Index));
+            return View(ingredientRecipe);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            IngredientRecipe ingredientRecipe = await service.GetIngredientRecipeById(id);
+            return View(ingredientRecipe);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ingredientId,recipeId,quantity,unit")] IngredientRecipe ingredientRecipe)
+        {
+            if (id != ingredientRecipe.recipeId && id != ingredientRecipe.ingredientId)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                service.Update(ingredientRecipe);
+            }
+            catch (DataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+            }
+
+            return RedirectToAction(nameof(Index));
+
+            return View(ingredientRecipe);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            IngredientRecipe ingredientRecipe = await service.GetIngredientRecipeById(id);
+            await service.Delete(ingredientRecipe.recipeId);
+            return View(ingredientRecipe);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            IngredientRecipe ingredientRecipe = await service.GetIngredientRecipeById(id);
+            service.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
